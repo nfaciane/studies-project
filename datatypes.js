439eb3aaa154004e1aad3a975fd82e9cbf53398b
Natalie F <nataliefaciane2015@gmail.com>
2025-11-23 00:27:17 -0600

    save it

--- datatypes.js
+++ datatypes.js
@@ -3,4 +3,57 @@
  * 
  * 0: (introduction)
  * 
+ * 1: infinity vs -infinity
+ * 
+ * 2: simple vs complex
+ * 
+ * 3: by copy vs by reference
  */
+
+// 1. number
+// how it works
+//code example
+
+// 2. string
+// how it works
+//code example
+
+// 3. boolean
+// how it works
+//code example
+
+// 4. array
+// how it works
+//code example
+
+// 5. object
+// how it works
+//code example
+
+// 6. function
+// how it works
+//code example
+
+// 7. undefined
+// how it works
+//code example
+
+// 8. null
+//how it works
+//code example
+
+// 9. NaN
+//how it works
+//code example
+
+// 10. infinity vs -infinity
+// how it works
+//code example
+
+// 11. simple vs complex data type
+//how it works
+//code example
+
+// 12. by copy vs by reference 
+// how does it work
+//code example
