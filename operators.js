1267d2d429a984f4577e84be2d92b073eaf106fb
Natalie F <nataliefaciane2015@gmail.com>
2025-11-23 00:32:42 -0600

    save

--- operators.js
+++ operators.js
@@ -3,4 +3,29 @@
  * 
  * 0: (introduction)
  * 
+ * 1: types of operators
  */
+
+// 1. assignment operators
+// how it works
+//code example
+
+// 2. arithmetic operators
+// how it works
+//code example
+
+// 3. comparison operators
+// how it works
+//code example
+
+// 4. logical operators
+// how it works
+//code example
+
+// 5. unary operators (!, typeof, -)
+// how it works
+//code example
+
+// 6. ternary operator (a ? b : c)
+// how it works
+//code example
