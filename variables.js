1275ca82fec7ba8858c99f776b835813bb82580e
Natalie F <nataliefaciane2015@gmail.com>
2025-11-23 00:17:48 -0600

    save it

--- variables.js
+++ variables.js
@@ -3,4 +3,20 @@
  * 
  * 0: (provide general description of the concept)
  * 
+ * 1: can be declared
+ * 
+ * 2: can be assigned
  */
+
+// 1. declaration, initalization, or assignment //
+// how does it work
+//code example
+
+// 2. var, let, const
+// how it works
+//code example
+
+// 3. hoisting
+// how it works 
+//code example
+
