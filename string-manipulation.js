0811c79635c90f0aa4664dcb8c83dd9e322d720c
Natalie F <nataliefaciane2015@gmail.com>
2025-11-23 00:35:01 -0600

    save it all

--- string-manipulation.js
+++ string-manipulation.js
@@ -4,3 +4,11 @@
  * 0: (introduction)
  * 
  */
+
+// 1. manipulating strings with operators
+// how it works
+//code example
+
+// 2. manipulating strings with string methods
+// how it works
+//code example
