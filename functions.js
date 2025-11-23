f5e6bf3a2c798168a8b01f63e4e740f04c03ff3e
Natalie F <nataliefaciane2015@gmail.com>
2025-11-23 00:45:26 -0600

    save

--- functions.js
+++ functions.js
@@ -4,3 +4,17 @@
  * 0: (introduction)
  * 
  */
+
+// 1. parts of a function (decalring vs calling)
+
+// 2. parameters vs arguments (passed)
+
+// 3. syntax for named function
+
+// 4. assigning a function to a variable
+
+// 5. specify function input vs specify function output
+
+// 6. function and scope (parent scope vs global scope)
+
+// 7. functions and closures
