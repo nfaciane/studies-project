7ba082fff1f51e3ce22c95f5b12c0b623c2f3e2f
Natalie F <nataliefaciane2015@gmail.com>
2025-11-24 00:06:54 -0600

    save

--- variables.js
+++ variables.js
@@ -1,22 +1,31 @@
 /**
  * VARIABLES:
  * 
- * 0: (provide general description of the concept)
+ * 0: To hold things in memory during the life cycle of our program, we
+ * can create variables. Variables are named identifiers that can point to 
+ * values of a particular type, like a Number, String, Array, Object, or another
+ * datatype. Variables are called so because we can change the value--and type of
+ * value--to which they point.
  * 
- * 1: can be declared
+ * 1. To create a variable we declare it using the keyword var, followed by a
+ * name (id or alias) for the variable.
  * 
- * 2: can be assigned
+ * 2. There are two phases of using variables: declaration and initialization (or
+ * assignment).
+ * 
+ * 3. Variables declared with var can be reassigned at any time.
+ * 
  */
 
-// 1. declaration, initalization, or assignment //
-// how does it work
-//code example
+// 1. Declaration //
+var myName; // variable declared but not assigned a value
+console.log(myName); // prints => undefined
 
-// 2. var, let, const
-// how it works
-//code example
+// 2. Initialization or Assignment //
+myName = 'John';
+console.log(myName); // prints => john
 
-// 3. hoisting
-// how it works 
-//code example
+// 3. Reassignment //
+myName = 'bob';
+console.log(myName); // prints => bob
 
